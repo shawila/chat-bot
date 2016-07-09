9827d7a4b529241b894dc327cf645e944d56b96e
Salah Hawila <salah.hawila@gmail.com>
2016-07-10 00:31:14 +0900

    initial commit

---
+++ config/initializers/discord.rb
@@ -0,0 +1,20 @@
+require 'discordrb'
+
+DISCORD_BOT_TOKEN = ENV['DISCORD_BOT_TOKEN']
+DISCORD_APP_ID = ENV['DISCORD_APP_ID']
+
+chat_bot = Discordrb::Bot.new token: DISCORD_BOT_TOKEN, application_id: DISCORD_APP_ID
+command_bot = Discordrb::Commands::CommandBot.new token: DISCORD_BOT_TOKEN, application_id: DISCORD_APP_ID, prefix: '!'
+
+chat_bot.message(with_text: "Hey Bot!") do |event|
+  event.respond "Hi, #{event.user.name}!"
+end
+
+command_bot.command :dice do |event, max|
+  max = 6 if max.nil? or max.to_i < 2
+  rand(1 .. max.to_i)
+end
+
+chat_bot.run :async
+command_bot.run :async
+
