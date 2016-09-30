5e1fc07c1aa5c567aa889d22985c6b4ffd7bb6b4
Salah Hawila <shawila@users.noreply.github.com>
2016-09-30 10:04:51 +0900

    small hot fix to raid time

--- app/services/omega_discord_bot.rb
+++ app/services/omega_discord_bot.rb
@@ -61,7 +61,7 @@ class OmegaDiscordBot
     if (now < raid_time)
       "Next raid starts in **#{hours} hours and #{minutes} minutes**!"
     elsif (now < zerg_time)
-      "Raid started **#{- hours} hours and #{- minutes} minutes** ago!\nZerg starts in **#{8 + hours} hours and #{60 + minutes} minutes**!"
+      "Raid started **#{- hours} hours and #{- minutes} minutes** ago!\nZerg starts in **#{36 + 8 + hours} hours and #{60 + minutes} minutes**!"
     else
       "Raid started **#{- hours} hours and #{- minutes} minutes** ago!\nZerg started **#{- hours - 9} hours and #{- minutes} minutes** ago!"
     end
